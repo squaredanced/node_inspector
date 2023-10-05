d54f009f5872202b9a864db35f417d9f50c48729
squaredanced@gmail.com <squaredanced@gmail.com>
2023-10-04 21:24:46 -0700

    __init__: changed order of reloads

--- __init__.py
+++ __init__.py
@@ -1,15 +1,19 @@
 from importlib import reload
+from . import utils
 from . import node_inspector_ui
 from . import printer
 from . import generate_wrapper
 from . import python_highlighter
 from . import widgets_construct
 
+
 # Reload modules
+reload(utils)
 reload(node_inspector_ui)
 reload(printer)
 reload(generate_wrapper)
 reload(widgets_construct)
+
 
 # Import into package namespace
 from . import node_inspector_ui as ni
