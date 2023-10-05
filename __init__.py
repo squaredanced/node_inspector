0ac629045ebbe827ddd2293758d3a5001fe630ec
squaredanced@gmail.com <squaredanced@gmail.com>
2023-10-04 20:43:06 -0700

    minor bump

--- __init__.py
+++ __init__.py
@@ -3,14 +3,16 @@ from . import node_inspector_ui
 from . import printer
 from . import generate_wrapper
 from . import python_highlighter
+from . import widgets_construct
 
 # Reload modules
 reload(node_inspector_ui)
 reload(printer)
 reload(generate_wrapper)
+reload(widgets_construct)
 
 # Import into package namespace
 from . import node_inspector_ui as ni
 from . import printer
 from . import generate_wrapper
-from . import python_highlighter
+from . import widgets_construct
