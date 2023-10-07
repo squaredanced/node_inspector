7a73dd7e2fc53379e06c63c9c57fd7b92244021b
squaredanced@gmail.com <squaredanced@gmail.com>
2023-10-07 00:06:45 -0700

    generate_wrapper: imporvements to generated code

--- generate_wrapper.py
+++ generate_wrapper.py
@@ -3,40 +3,42 @@ import hou
 print("Also imported from a folder!")
 
 
-def generate_properties(node, indent=0):
-    """
-    Generate Python property and setter methods for a given hou.Node's parameters.
+def generate_properties(node):
+    class_name = "".join(word.title() for word in node.name().split("_"))
+    code = f"class {class_name}Wrapper:\n    def __init__(self, node):\n        self.node = node\n"
 
-    Args:
-        node (hou.Node): The Houdini node for which to generate the properties.
-        indent (int): Current indentation level.
-
-    Returns:
-        str: The string containing the Python property and setter methods.
-    """
-    output_code = ""
     parm_template_group = node.parmTemplateGroup()
-    parms = parm_template_group.entries()
+    folder_parms = {}
 
-    for parm_template in parms:
+    for parm_template in parm_template_group.entries():
         if isinstance(parm_template, hou.FolderParmTemplate):
-            output_code += generate_properties_for_folder(parm_template, indent + 1)
+            folder_name = parm_template.name()
+            folder_parms[folder_name] = parm_template.parmTemplates()
         else:
+            folder_parms["root"] = folder_parms.get("root", [])
+            folder_parms["root"].append(parm_template)
+
+    for folder, parms in folder_parms.items():
+        if folder != "root":
+            code += f"    # from folder {folder}\n"
+
+        for parm_template in parms:
+            if isinstance(
+                parm_template, (hou.SeparatorParmTemplate, hou.FolderParmTemplate)
+            ):
+                continue
+
             parm_name = parm_template.name()
-            property_code = f"""
+            code += f"""
     @property
     def {parm_name}(self):
-        return self.node.parm("{parm_name}").eval()
-"""
+        return self.node.parm('{parm_name}').eval()
 
-            setter_code = f"""
     @{parm_name}.setter
     def {parm_name}(self, value):
-        self.node.parm("{parm_name}").set(value)
+        self.node.parm('{parm_name}').set(value)
 """
-            output_code += property_code + setter_code
-
-    return output_code
+    return code
 
 
 def generate_properties_for_folder(folder_parm_template, indent):
