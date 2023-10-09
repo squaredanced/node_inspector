7072b84847f3d566f6bf508adc515c263bdbd45e
squaredanced@gmail.com <squaredanced@gmail.com>
2023-10-08 21:28:11 -0700

    edit_widget: add change size buttons

--- edit_widget.py
+++ edit_widget.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QTextEdit, QWidget, QVBoxLayout
+from PySide2.QtWidgets import QTextEdit, QWidget, QVBoxLayout, QPushButton
 from PySide2.QtGui import QTextOption
 from .python_highlighter import PythonHighlighter
 from .widgets_construct import NeatWidgetConstructor, NeatLayoutTypes
@@ -17,8 +17,9 @@ class EditWidget(QWidget):
 
         self.edit_text_widget = QTextEdit()
         self.setup_text_edit()
-        self.main_widget.add_widget(self.edit_text_widget)
+        self.main_widget.add_widget(self.edit_text_widget, stretch=1)
         self.layout.addWidget(self.main_widget)
+        self.add_text_size_controls()
         self.setLayout(self.layout)
 
         self.syntax_highlighter = PythonHighlighter(self.edit_text_widget.document())
@@ -33,6 +34,40 @@ class EditWidget(QWidget):
         self.edit_text_widget.setLineWrapColumnOrWidth(600)
         self.edit_text_widget.setWordWrapMode(QTextOption.NoWrap)
         self.edit_text_widget.setStyleSheet("background-color: rgb(5,5,5);")
+
+    def add_text_size_controls(self):
+        self.buttons_holder = NeatWidgetConstructor(
+            layout_type=NeatLayoutTypes.HORIZONTAL, add_stretch=True
+        )
+        self.buttons_holder.add_widget(self.create_button("+", self.increase_font_size))
+        self.buttons_holder.add_widget(self.create_button("-", self.decrease_font_size))
+        self.main_widget.add_widget(self.buttons_holder, stretch=0)
+
+    def create_button(self, text, callback):
+        button = QPushButton(text)
+        button.clicked.connect(callback)
+        return button
+
+    def refresh_highlighting(self):
+        cursor_position = self.edit_text_widget.textCursor().position()
+        content = self.edit_text_widget.toPlainText()
+        self.edit_text_widget.setPlainText("")  # Clear text
+        self.edit_text_widget.setPlainText(content)  # Reset text
+        cursor = self.edit_text_widget.textCursor()
+        cursor.setPosition(cursor_position)
+        self.edit_text_widget.setTextCursor(cursor)
+
+    def increase_font_size(self):
+        font_size = self.edit_text_widget.fontPointSize()
+        self.edit_text_widget.setFontPointSize(font_size + 1)
+        self.syntax_highlighter.rehighlight()
+        self.refresh_highlighting()
+
+    def decrease_font_size(self):
+        font_size = self.edit_text_widget.fontPointSize()
+        self.edit_text_widget.setFontPointSize(font_size - 1)
+        self.syntax_highlighter.rehighlight()
+        self.refresh_highlighting()
 
     def __getattr__(self, name):
         """Delegate attribute access to the inner QTextEdit."""
