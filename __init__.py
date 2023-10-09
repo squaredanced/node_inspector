500f380ad0f1d312e3ca07d682566dbb33b31393
squaredanced@gmail.com <squaredanced@gmail.com>
2023-10-08 21:50:35 -0700

    __init__: temp dynamic import adjustments

--- __init__.py
+++ __init__.py
@@ -7,10 +7,12 @@ from . import python_highlighter
 from . import widgets_construct
 from . import explode_hda_to_subnet
 from . import get_all_labels
+from . import edit_widget
 
 
 # Reload modules
 reload(utils)
+reload(edit_widget)
 reload(explode_hda_to_subnet)
 reload(get_all_labels)
 reload(node_inspector_ui)
