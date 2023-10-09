b4976bccab77ff691dbb077c2170aefc5b5aa317
squaredanced@gmail.com <squaredanced@gmail.com>
2023-10-08 23:48:57 -0700

    __init__: dev update

--- __init__.py
+++ __init__.py
@@ -1,9 +1,8 @@
 from importlib import reload
+from . import constants
 from . import utils
 from . import node_inspector_ui
-from . import printer
 from . import generate_wrapper
-from . import python_highlighter
 from . import widgets_construct
 from . import explode_hda_to_subnet
 from . import get_all_labels
@@ -11,18 +10,17 @@ from . import edit_widget
 
 
 # Reload modules
+reload(constants)
 reload(utils)
 reload(edit_widget)
 reload(explode_hda_to_subnet)
 reload(get_all_labels)
 reload(node_inspector_ui)
-reload(printer)
 reload(generate_wrapper)
 reload(widgets_construct)
 
 
 # Import into package namespace
 from . import node_inspector_ui as ni
-from . import printer
 from . import generate_wrapper
 from . import widgets_construct
