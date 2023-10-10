dba8fdfda8b2170199121bf19b804e31ca552462
squaredanced@gmail.com <squaredanced@gmail.com>
2023-10-09 17:28:17 -0700

    node_inspector_ui: removed fucnction connection logic Removed populate buttons. Update imports

--- node_inspector_ui.py
+++ node_inspector_ui.py
@@ -1,14 +1,14 @@
 import hou
-from functools import partial
 from .constants import BG_COLOR
-from .generate_wrapper import generate_properties
-from .get_all_labels import traverse_parms_from_node
+
 from .edit_widget import EditWidget
-from .utils import pretty_print_dict, node_validator, ParmInfo
-from .explode_hda_to_subnet import explode_me
+from .populate_buttons import populate_buttons
+from .utils import node_validator
+from .button_callback_manager import BUTTON_MAPPING
+
 from .widgets_construct import NeatWidgetConstructor, NeatLayoutTypes
+from . import style
 from PySide2.QtWidgets import (
-    QPushButton,
     QWidget,
     QTabWidget,
     QMainWindow,
@@ -23,165 +23,9 @@ from PySide2.QtGui import (
     QDragMoveEvent,
 )
 
-# from .generate_wrapper import generate_properties
-
-
-def text_edit_handler(node, text_edit, text=""):
-    text_edit.clear()
-    text_edit.append(text)
-
-
-def get_user_data(node, text_edit):
-    node_data = node.userDataDict()
-    text = pretty_print_dict(node_data)
-    text_edit_handler(node, text_edit, text)
-
-
-def get_labels(node, text_edit):
-    text = traverse_parms_from_node(node)
-
-    text_edit_handler(node, text_edit, text)
-
-
-def get_all_defaults(node, text_edit):
-    text = pretty_print_dict(ParmInfo(node).get_parm_default_values(), indent=1)
-    text_edit_handler(node, text_edit, text)
-
-
-def get_all_expressions(node, text_edit):
-    text = pretty_print_dict(
-        ParmInfo(node).get_parm_expressions(include_hidden=True), indent=1
-    )
-    text_edit_handler(node, text_edit, text)
-
-
-def get_all_conditionals(node, text_edit):
-    text = pretty_print_dict(
-        ParmInfo(node).get_parm_conditionals(include_hidden=True), indent=1
-    )
-    text_edit_handler(node, text_edit, text)
-
-
-def generate_wrapper(node, text_edit):
-    text = generate_properties(node)
-    text_edit_handler(node, text_edit, text)
-
-
-def explode_to_subnetwork(node, text_edit):
-    explode_me(node)
-    text_edit_handler(
-        node, text_edit, text=f"Exploded node:\n\t{node.path()}\nto Subnetwork"
-    )
-
-
-def get_all_callbacks(node, text_edit):
-    text = pretty_print_dict(ParmInfo(node).get_parm_callbacks(), indent=1)
-    text_edit_handler(node, text_edit, text)
-
-
-# Create a mapping between button names and functions
-BUTTON_MAPPING = {
-    "Get User Data": get_user_data,
-    "Get Labels": get_labels,
-    "Get All Defaults": get_all_defaults,
-    "Get All Expressions": get_all_expressions,
-    "Get All Callbacks": get_all_callbacks,
-    "Get All Conditionals": get_all_conditionals,
-    "Generate Wrapper": generate_wrapper,
-    "Explode To Subnetwork": explode_to_subnetwork,
-}
 
 MARGIN = 5
 PADDING = 15
-
-
-def populate_buttons(
-    sample_list,
-    buttons_list,
-    layout,
-    callback,
-    checkable=False,
-    width=30,
-    fixed_width=False,
-    uncheck=True,
-    initial_checked=1,
-    margin=5,
-    padding=15,
-):
-    """Populate a layout with checkable buttons from list.
-    Connects each button to a callback function.
-    Pass the button value to the callback function.
-
-    Args:
-        sample_list (list): list of values to be used as button names
-        buttons_list (list): list of buttons to be populated
-        layout (QLayout): layout to be populated
-        callback (function): function to be called on button press
-        checkable (bool, optional): whether the button is checkable. Defaults to True.
-        width (int, optional): width of the button. Defaults to 30.
-        fixed_width (bool, optional): whether the button width is fixed. Defaults to True.
-        uncheck (bool, optional): whether to uncheck other buttons when one is pressed. Defaults to True.
-
-    Raises:
-        TypeError: if buttons_list is not a list
-        Warning: if sample_list is None or empty
-        TypeError: if sample_list is not a list
-
-    """
-
-    def set_unchecked(button, buttons):
-        """Set all buttons in a list to unchecked, except the one that was pressed
-
-        Args:
-            button (QPushButton): button that was pressed
-            buttons (list): list of buttons to be unchecked
-        """
-        for b in buttons:
-            if b != button:
-                b.setChecked(False)
-
-    # properly handle mutable default arguments
-    # raise an error if buttons_list is not list
-    if buttons_list is None:
-        buttons_list = []
-
-    if not isinstance(buttons_list, list):
-        raise TypeError("buttons_list must be a list")
-
-    # handle sample_list being None or empty
-    if sample_list is None or len(sample_list) == 0:
-        # raise warning if sample_list is None or empty
-        raise Warning("sample_list is None or empty")
-
-    # raise error if sample_list is not list
-    if not isinstance(sample_list, list):
-        raise TypeError("sample_list must be a list")
-
-    for n, i in enumerate(sample_list):
-        button = QPushButton(" ".join(str(i).split("_")).title())
-        button.setStyleSheet(
-            "QPushButton {background-color: rgb(10,10,10);"
-            "color: rgb(200,200,200); border-radius: 10px; padding: 20px; margin: 5px;}"
-            "QPushButton:hover {background-color: rgb(65,85,130);}"
-            "QPushButton:checked {background-color: rgb(80,95,180);}"
-            "QPushButton:pressed {background-color: rgb(80,95,180);}"
-        )
-        if checkable:
-            button.setCheckable(True)
-        if n == initial_checked and checkable:
-            button.setChecked(True)
-        if fixed_width:
-            button.setFixedWidth(width)
-        buttons_list.append(button)
-        button.pressed.connect(partial(callback, i))
-        layout.addWidget(button)
-
-    # If button is pressed, set all other buttons to unchecked
-    if uncheck:
-        for button in buttons_list:
-            button.pressed.connect(
-                lambda button=button: set_unchecked(button, buttons_list)
-            )
 
 
 class NodePathField(QWidget):
@@ -247,10 +91,14 @@ class MainWIndow(QMainWindow):
         self.node_path_field = NodePathField(main_window=self)
         buttons_widget.add_widget(self.node_path_field)
 
+        # Add tabs
         self.tabs = QTabWidget()
+        self.tabs.setTabsClosable(True)
+        self.tabs.setStyleSheet(style.tab_style)
 
         self.node_edit_widgets = {}
 
+        # Add buttons
         populate_buttons(
             sample_list=[i for i in BUTTON_MAPPING.keys()],
             buttons_list=self.buttons_list,
@@ -260,16 +108,17 @@ class MainWIndow(QMainWindow):
             initial_checked=-1,
         )
 
+        # Add buttons to layout
         splitter.addWidget(buttons_widget)
 
-        splitter.addWidget(buttons_widget)
-
+        # Connect tabs changed signal
         self.tabs.currentChanged.connect(self.on_tab_changed)
-        splitter.addWidget(self.tabs)
 
+        splitter.addWidget(self.tabs)
         splitter.setStretchFactor(0, 0)  # buttons_widget, static
         splitter.setStretchFactor(1, 1)  # self.edit_text_widget, stretches
 
+        # Set the splitter as the central widget
         self.central_widget.main_layout.addWidget(splitter)
 
     def create_edit_text(self):
